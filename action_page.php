7194b08410ae7b1e9739322fb6a2aae35cd76e99
Евгений Рейников <gendos2411@bk.ru>
2021-12-05 20:34:45 +0300

    проверка на пустое поле ввода

--- action_page.php
+++ action_page.php
@@ -1,6 +1,8 @@
 <?php
-$array = [
-    'name' => $_GET["name"], 'length' => mb_strlen($_GET["name"])
-];
+if ($_GET["name"] != '') {
+    $array = [
+        'name' => $_GET["name"], 'length' => mb_strlen($_GET["name"])
+    ];
+}
 require_once('form.html');
 
