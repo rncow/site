1a8edd03024228428c4ad90ce0e3a84a4f5b67e5
Евгений Рейников <gendos2411@bk.ru>
2021-12-07 16:46:42 +0300

    код из action_page.php перенесён в form.php, сам action_page.php удалён

--- action_page.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_GET["name"] != '') {
-    $array = [
-        'name' => htmlspecialchars($_GET["name"]), 'length' => mb_strlen($_GET["name"])
-    ];
-}
-require_once('form.php');
-
--- form.php
+++ form.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-<form action="action_page.php" method="get">
+<form action="" method="get">
     <label for="name">Name:</label><br>
     <input type="text" id="name" name="name"><br>
     <input type="submit" value="Отправить">
@@ -15,10 +15,13 @@
 <?php
 if (isset($_GET["name"])) {
     if (($_GET["name"]) != '') {
+        $array = [
+            'name' => htmlspecialchars($_GET["name"]),
+            'length' => mb_strlen($_GET["name"])
+        ];
         print("Имя: " . $array['name'] . ", кол-во символов: " . $array['length']);
     } else print "Имя не было введено.";
 }
-
 ?>
 
 </body>
