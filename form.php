abe2bfe2c574e569095cb4790fa986333d77c892
Евгений Рейников <gendos2411@bk.ru>
2021-12-08 18:29:14 +0300

    работа с меткой времени Unix; submit появляется только тогда, когда в обоих полях есть значение, и исчезает, если хотя бы в одном значение не указано

--- form.php
+++ form.php
@@ -7,14 +7,18 @@
 <body>
 
 <form action="" method="get">
-    <label for="name">Name:</label><br>
+    <label for="name">Введите имя:</label><br>
     <input type="text" id="name" name="name"><br>
+    <label for="date">Введите дату:</label><br>
+    <input type="date" id="date" name="date"><br>
     <input hidden type="submit" id="submit" value="Отправить">
 </form>
+
 <script>
     document.getElementById('name').addEventListener('change', displayButton);
+    document.getElementById('date').addEventListener('change', displayButton);
     function displayButton() {
-        if (document.getElementById('name').value != '') {
+        if (document.getElementById('name').value != '' && document.getElementById('date').value != '') {
             document.getElementById('submit').hidden = false;
         }
         else {
@@ -24,14 +28,26 @@
 </script>
 
 <?php
-if (isset($_GET["name"])) {
+if (isset($_GET["name"]) && isset($_GET["date"])) {
     if (($_GET["name"]) != '') {
         $array = [
             'name' => htmlspecialchars($_GET["name"]),
             'length' => mb_strlen($_GET["name"])
         ];
-        print("Имя: " . $array['name'] . ", кол-во символов: " . $array['length']);
+        print("Имя: " . $array['name'] . ", кол-во символов: " . $array['length'] . '<br>');
     } else print "Имя не было введено.";
+
+    if (($_GET["date"]) != '') {
+        $monthsList = [".01." => "января", ".02." => "февраля",
+            ".03." => "марта", ".04." => "апреля", ".05." => "мая", ".06." => "июня",
+            ".07." => "июля", ".08." => "августа", ".09." => "сентября",
+            ".10." => "октября", ".11." => "ноября", ".12." => "декабря"];
+
+        $date_from_form = date("d.m.Y", strtotime($_GET["date"]));
+        $replace = date(".m.", strtotime($_GET["date"]));
+        $date_from_form = str_replace($replace, " ".$monthsList[$replace]." ", $date_from_form);
+        print ("Введённая дата: " . $date_from_form);
+    } else print "Дата не была введена.";
 }
 ?>
 
