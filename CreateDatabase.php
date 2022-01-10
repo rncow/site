ddb0165a515aefb3f904927f6fac2964e22af25b
Евгений Рейников <gendos2411@bk.ru>
2022-01-10 16:59:27 +0300

    дабовлены CreateDatabase.php и CreateTableNews.php

--- CreateDatabase.php
+++ CreateDatabase.php
@@ -1,19 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Создание БД</title>
+    </head>
+    <body>
+        <form action="" method="get">
+            <input type="button" name="button" value="Создать">
+        </form>
+    </body>
+</html>
+
 <?php
+if (isset($_GET['button'])) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
+    try {
+        $conn = new PDO("mysql:host=$servername", $username, $password);
+        // установка режима сообщений о ошибках PDO: Выбрасывать исключения.
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE MyDB";
+        // использование exec(), т.к. никаких результатов не возвращается
+        $conn->exec($sql);
+        echo "Database created successfully<br>";
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
 
-try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
-    // установка режима сообщений о ошибках PDO: Выбрасывать исключения.
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE MyDB";
-    // использование exec(), т.к. никаких результатов не возвращается
-    $conn->exec($sql);
-    echo "Database created successfully<br>";
-} catch (PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+    $conn = null;
 }
-
-$conn = null;
--- CreateTableNews.php
+++ CreateTableNews.php
@@ -1,27 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Создание таблицы</title>
+</head>
+<body>
+<form action="" method="get">
+    <input type="button" name="button" value="Создать">
+</form>
+</body>
+</html>
+
 <?php
+if (isset($_GET['button'])) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "MyDB";
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "MyDB";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // установка режима сообщений о ошибках PDO: Выбрасывать исключения.
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // установка режима сообщений о ошибках PDO: Выбрасывать исключения.
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE news (
+        id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(200) NOT NULL,
+        text TEXT NOT NULL,
+        date DATETIME NOT NULL
+        )";
 
-    $sql = "CREATE TABLE news (
-    id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(200) NOT NULL,
-    text TEXT NOT NULL,
-    date DATETIME NOT NULL
-    )";
+        // использование exec(), т.к. никаких результатов не возвращается
+        $conn->exec($sql);
+        echo "Table MyGuests created successfully<br>";
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
 
-    // использование exec(), т.к. никаких результатов не возвращается
-    $conn->exec($sql);
-    echo "Table MyGuests created successfully<br>";
-} catch (PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+    $conn = null;
 }
-
-$conn = null;
